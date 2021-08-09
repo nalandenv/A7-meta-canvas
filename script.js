a250150a9a68337878700bcc41e063ca04686649
Nikhil Nalande <nalandenv@gmail.com>
2021-08-09 18:00:50 +0530

    add canvas gradient

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const drawFirst = () => {
   const first = document.querySelector("#first");
-  const canFirst = first.getContext("2d");
+  const firstCanvas = first.getContext("2d");
 
-  canFirst.font = "50px Comic Neue";
-  canFirst.strokeText("Nikhil", 80, 80);
+  firstCanvas.font = "50px Comic Neue";
+  firstCanvas.strokeText("Nikhil", 80, 80);
+};
+
+const drawSecond = () => {
+  const second = document.querySelector("#second");
+  const secondCanvas = second.getContext("2d");
+
+  const canvasGradient = secondCanvas.createLinearGradient(0, 0, 150, 0);
+  canvasGradient.addColorStop(0.33, "orange");
+  canvasGradient.addColorStop(0.66, "white");
+  canvasGradient.addColorStop(1, "green");
+
+  secondCanvas.fillStyle = canvasGradient;
+  secondCanvas.fillRect(10, 10, 150, 80);
 };
 
 drawFirst();
+drawSecond();
